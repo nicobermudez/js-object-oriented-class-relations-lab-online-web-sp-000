7203c60ba1526e6801223cdcc071cf987312450e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 16:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class Passenger {
   }
   trips() {
     return store.trips.filter(function(trip) {
-      return trip.passengerId === this.tripId
+      return trip.passengerId === this.id
     }.bind(this))
   }
 
