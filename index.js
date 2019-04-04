baaedea8f89a6b6a90ee28e6d75106ba99d47616
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 16:45:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ let store = {users: [], passengers: [], trips: []}
 let userId = 0
 let passengerId = 0
 let tripId = 0
+
+
 class Driver {
   constructor(name) {
+    this.id = ++userId
     this.name = name
   }
 
